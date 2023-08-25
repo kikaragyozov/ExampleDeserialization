a8154e0e3a60866a5017d5a5dcd4afe4dec07d81
Kiril Karagyozov <spiritbober@gmail.com>
2023-08-25 15:56:30 +0300

    Add files via upload

---
+++ ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleDeserialization
+{
+    [ApiController]
+    [Route("test")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Test(ExampleObject @object)
+        {
+            return Ok(@object.SomeProperty);
+        }
+    }
+
+    public class ExampleObject
+    {
+        public string? SomeProperty { get; set; }
+    }
+}
